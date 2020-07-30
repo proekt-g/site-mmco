98efe00d1d181fe48f052a961e12457d82d8a630
Armen <armen.armen.armen95@gmail.com>
2020-07-30 23:38:44 +0300

    30.07.20

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,18 @@
-window.addEventListener('load', function() {});
+window.addEventListener('load', function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        speed: 400,
+        spaceBetween: 100,
+        slidesPerView: 7,
+        spaceBetween: 50,
+        // slidesPerGroup: 1,
+        slidesOffsetBefore: 50,
+        autoplay: {
+            delay: 2000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next img',
+            prevEl: '.swiper-button-prev img',
+        },
+    });
+
+});
