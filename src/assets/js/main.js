bea60afa969a3b8c1a9e13d34e6399eea41975ea
Armen <armen.armen.armen95@gmail.com>
2020-08-01 00:18:24 +0300

    31.07.20

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,40 @@
 window.addEventListener('load', function() {
-    var mySwiper = new Swiper('.swiper-container', {
-        speed: 400,
-        spaceBetween: 100,
-        slidesPerView: 7,
-        spaceBetween: 50,
-        // slidesPerGroup: 1,
-        slidesOffsetBefore: 50,
-        autoplay: {
-            delay: 2000,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next img',
-            prevEl: '.swiper-button-prev img',
-        },
-    });
 
+    let mySwiper;
+    let swiperRegions;
+
+    function checkWindowWidth() {
+        if (window.innerWidth <= 500) {
+            swiperRegions = createSlider('.regions-inner', 400, 'auto', 25, true, { delay: 2000 }, { nextEl: '.regions-slider-button-next', prevEl: '.regions-slider-button-prev' }, {}, 'regions-block', 'regions-block__box', 15);
+            // document.querySelector('.regions-block').style.transform = 'translate3d(0px, 0px, 0px)';
+            // if (mySwiper !== undefined) destroySlider(mySwiper);
+        }
+        if (window.innerWidth > 500) {
+            mySwiper = createSlider('.swiper-container', 400, 'auto', 25, true, { delay: 2000 }, { nextEl: '.slider-button-next', prevEl: '.slider-button-prev' }, { 500: { spaceBetween: 50 } });
+            // if (swiperRegions !== undefined) destroySlider(swiperRegions);
+        }
+    }
+
+    function destroySlider(sliderSwiper) {
+        sliderSwiper.destroy(true, true);
+    }
+
+    function createSlider(sliderSwiper, _speed, _slidesPerView, _spaceBetween, _loop, _autoplay, _navigation, _breakpoints, _wrapperClass = 'swiper-wrapper', _slideClass = 'swiper-slide', _slidesOffsetBefore = 0) {
+        return new Swiper(sliderSwiper, {
+            speed: _speed,
+            slidesPerView: _slidesPerView,
+            spaceBetween: _spaceBetween,
+            loop: _loop,
+            autoplay: _autoplay,
+            navigation: _navigation,
+            breakpoints: _breakpoints,
+            wrapperClass: _wrapperClass,
+            slideClass: _slideClass,
+            slidesOffsetBefore: _slidesOffsetBefore
+        });
+    }
+
+    checkWindowWidth();
+
+    window.addEventListener('resize', checkWindowWidth);
 });
